f74a3a6d07210ce8edf2751d23fee29f6004acb3
lil-12 <90734020+lil-12@users.noreply.github.com>
2021-10-18 20:33:58 -0400

    Update Test_changePassword_Helper.java

--- Test_changePassword_Helper.java
+++ Test_changePassword_Helper.java
@@ -7,15 +7,19 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
-public class Test_changePassword_Helper {
+public class Test_changePassword_Helper 
+{
 
-	public static void main (String args[]) {
+	public static void main (String args[]) 
+	{
 		
-		try {
+		try 
+		{
 			ArrayList<Account> accounts = new ArrayList<Account>();
-			AdminAccount acc1 = new AdminAccount("admin", "password", accounts);
+			AdminAccount acc1 = new AdminAccount("admin", "password", accounts);//account that password is beng changed for
 			
-			for (int i = 0; i < 2; i++) {
+			for (int i = 0; i < 2; i++) 
+			{
 				ServerSocket serve = new ServerSocket(52431);
 				Socket connection = serve.accept();
 				System.out.println("accepted connection");
@@ -24,13 +28,15 @@ public class Test_changePassword_Helper {
 				BufferedReader incoming = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 			
 				
-				StoreServer.changePassword(acc1, incoming, outgoing);
+				StoreServer.changePassword(acc1, incoming, outgoing);//calls changePasswordScene for test to communicate with
 				serve.close();
 				connection.close();
 			}	
 			
 			
-		} catch (Exception e) {
+		} 
+		catch (Exception e) 
+		{
 			e.printStackTrace();
 		}
 	}
